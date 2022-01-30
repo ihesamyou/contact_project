119afffb357a981401e326ef5dac3260146ab4e0
ihosseinu <ihosseinu@gmail.com>
2022-01-30 23:33:12 +0330

    contact model tests modified

--- test_project/contacts/tests.py
+++ test_project/contacts/tests.py
@@ -5,13 +5,14 @@ class ContactModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = Contact.objects.create(first_name='Test', phone_number='09137778899')
+        cls.user = Contact.objects.create(first_name='Testyahoo', phone_number='09137778899')
+        cls.test_user = Contact.objects.get(phone_number='09137778899')
 
     def test_required_fields(self):
-        self.assertEqual(self.user.first_name, 'Test')
-        self.assertEqual(self.user.phone_number, '09137778899')
+        self.assertEqual(self.test_user.first_name, 'Testyahoo')
+        self.assertEqual(self.test_user.phone_number, '09137778899')
 
     def test_phone_validation(self):
-        self.user.phone_number=='0913444111'
-        self.assertNotEqual(self.user.phone_number, '0913444111')
+        self.user.phone_number = '0913444111'
+        self.assertNotEqual(self.test_user.phone_number, '0913444111')
         
