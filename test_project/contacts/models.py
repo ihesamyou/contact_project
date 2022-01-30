7e2165d2dd4254c44726e5d5c68ec0941745a440
ihosseinu <ihosseinu@gmail.com>
2022-01-30 23:19:49 +0330

    contact model test added

--- test_project/contacts/models.py
+++ test_project/contacts/models.py
@@ -11,8 +11,8 @@ class Contact(models.Model):
     # For phone numbers, CharField is used to avoid having any problem in case of
     # having a number starting with 0. we validate phone number with phone_regex.
     # other numbers are are not validated because of their complexity.
-    phone_regex = RegexValidator(regex=r'(\+/d/d)?\d{10,11}', message='Phone number should be like one of theses formats: +989135556677 or 09135556677')
-    phone_number = models.CharField(unique=True, verbose_name='Phone', validators=[phone_regex])
+    phone_regex = RegexValidator(regex=r'\d{11}', message='Please enter a valid phone number.')
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name='Phone', validators=[phone_regex], help_text='Phone number should be like this format: 09135556677')
     home = models.CharField(blank=True, null=True, max_length=16)
     work = models.CharField(blank=True, null=True, max_length=16)
     email = models.EmailField(unique=True, blank=True, null=True)
@@ -20,3 +20,6 @@ class Contact(models.Model):
     address = models.CharField(blank=True, null=True, max_length=300)
     emergency_contact = models.BooleanField(default=False, help_text='Call this contact in case of emergency.')
     notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.phone_number}'
--- test_project/contacts/tests.py
+++ test_project/contacts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Contact
 
-# Create your tests here.
+class ContactModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = Contact.objects.create(first_name='Test', phone_number='09137778899')
+
+    def test_required_fields(self):
+        self.assertEqual(self.user.first_name, 'Test')
+        self.assertEqual(self.user.phone_number, '09137778899')
+
+    def test_phone_validation(self):
+        self.user.phone_number=='0913444111'
+        self.assertNotEqual(self.user.phone_number, '0913444111')
+        
